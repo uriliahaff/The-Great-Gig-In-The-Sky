e3f5694d7188fab991b71304bd7e502546a038cf
uriliahaff <liahaffuriel@gmail.com>
2023-04-21 15:04:34 -0300

    Prittier

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,43 @@
-import './App.css'
-import Navbar from './components/Navbar'
-import ItemListContainer from './components/ItemListContainer'
-import { Routes, Route, Link } from 'react-router-dom'
+import "./App.css";
+import Navbar from "./components/Navbar";
+import ItemListContainer from "./components/ItemListContainer";
+import { Routes, Route, Link } from "react-router-dom";
 
-import { 
-  ChakraProvider,
-} from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
+import Home from "./components/Home";
+import ItemDetails from "./components/ItemDetails";
+import Checkout from "./components/Checkout";
+import React from "react";
+import Footer from "./components/Footer";
 
-
-import Home from './components/Home';
-import ItemDetails from './components/ItemDetails';
-import Checkout from './components/Checkout';
-import React from 'react';
-import Footer from './components/Footer';
-
-import { CartProvider } from './contexts/CartContext';
-import Order from './components/Order';
-
+import { CartProvider } from "./contexts/CartContext";
+import Order from "./components/Order";
 
 function App() {
-
   return (
     <CartProvider>
-    <ChakraProvider>
+      <ChakraProvider>
         <div>
-          <Navbar logo="https://www.linkpicture.com/q/logo_744.png"/>
+          <Navbar logo="https://www.linkpicture.com/q/logo_744.png" />
         </div>
 
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<ItemListContainer />} />
+          <Route path="/items" element={<ItemListContainer />} />
+          <Route path="/category/:id" element={<ItemListContainer />} />
+          <Route path="/items/:id" element={<ItemDetails />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/order" element={<Order />} />
 
-    <Routes>
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/" element={<ItemListContainer/>}/>
-        <Route path="/items" element={<ItemListContainer/>}/>
-        <Route path='/category/:id' element={<ItemListContainer />}/>
-        <Route path='/items/:id' element={<ItemDetails/>}/>
-        <Route path='/checkout' element={<Checkout/>}/>
-        <Route path='/order' element={<Order/>}/>
+          <Route path="/404" element={<h2>Error 404: Not founrd!</h2>} />
+        </Routes>
 
-        <Route path='/404' element={<h2>Error 404: Not founrd!</h2>}/>
-    </Routes>
-
-    <Footer/>
-    </ChakraProvider>
+        <Footer />
+      </ChakraProvider>
     </CartProvider>
-
-  )
+  );
 }
 
-export default App
+export default App;
--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -1,103 +1,109 @@
 import { createContext, useState } from "react";
-import { useToast } from '@chakra-ui/react'
-export const CartContext = createContext()
+import { useToast } from "@chakra-ui/react";
+export const CartContext = createContext();
 
-export const CartProvider = ({children}) => {
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+  const toast = useToast();
 
-    const [cart, setCart] = useState([]);
-    const toast = useToast()
+  const addItem = (item) => {
+    const existingItem = cart.find((cart) => cart.id === item.id);
 
-    const addItem = (item) => {
-      const existingItem = cart.find((cart) => cart.id === item.id);
-
-
-      if (existingItem) {
-        setCart((prevCartItems) =>
-          prevCartItems.map((cart) =>
-          cart.id === item.id
-              ? { ...cart, quantity: cart.quantity + 1 }
-              : cart
-          )
-        );
-      } else {
-        setCart((prevCartItems) => [
-          ...prevCartItems,
-          { id: item.id, name: item.name, price: item.price, image: item.image, quantity: 1 },
-        ]);
-      }
-        console.log("Vamos a agregar el item: "+item.name);
-        toast({
-          title: 'Item agregado.',
-          description: item.name,
-          status: 'success',
-          duration: 2000,
-          position: 'top-right',
-          isClosable: true,
-        })
-
-      };
-
-      const removeItem = (itemId) => {
-        const updatedCartItems = cart.map((cart) =>
-        cart.id === itemId
-          ? { ...cart, quantity: cart.quantity - 1, totalPrice: cart.totalPrice - cart.price }
-          : cart
-      ).filter((cart) => cart.quantity > 0);
-      setCart(updatedCartItems);
-      };
-
-      const clearCart = () => {
-        setCart([]);
-      };
-    
-      const incrementItem = (itemId) => {
-        setCart((prevCartItems) =>
-          prevCartItems.map((cart) =>
-          cart.id === itemId
-              ? { ...cart, quantity: cart.quantity + 1 }
-              : cart
-          )
-        );
-      };
-    
-      const decrementItem = (itemId) => {
-        setCart((prevCartItems) =>
+    if (existingItem) {
+      setCart((prevCartItems) =>
         prevCartItems.map((cart) =>
+          cart.id === item.id ? { ...cart, quantity: cart.quantity + 1 } : cart
+        )
+      );
+    } else {
+      setCart((prevCartItems) => [
+        ...prevCartItems,
+        {
+          id: item.id,
+          name: item.name,
+          price: item.price,
+          image: item.image,
+          quantity: 1,
+        },
+      ]);
+    }
+    console.log("Vamos a agregar el item: " + item.name);
+    toast({
+      title: "Item agregado.",
+      description: item.name,
+      status: "success",
+      duration: 2000,
+      position: "top-right",
+      isClosable: true,
+    });
+  };
+
+  const removeItem = (itemId) => {
+    const updatedCartItems = cart
+      .map((cart) =>
+        cart.id === itemId
+          ? {
+              ...cart,
+              quantity: cart.quantity - 1,
+              totalPrice: cart.totalPrice - cart.price,
+            }
+          : cart
+      )
+      .filter((cart) => cart.quantity > 0);
+    setCart(updatedCartItems);
+  };
+
+  const clearCart = () => {
+    setCart([]);
+  };
+
+  const incrementItem = (itemId) => {
+    setCart((prevCartItems) =>
+      prevCartItems.map((cart) =>
+        cart.id === itemId ? { ...cart, quantity: cart.quantity + 1 } : cart
+      )
+    );
+  };
+
+  const decrementItem = (itemId) => {
+    setCart((prevCartItems) =>
+      prevCartItems
+        .map((cart) =>
           cart.id === itemId && cart.quantity > 1
             ? { ...cart, quantity: cart.quantity - 1 }
             : cart.id === itemId && cart.quantity === 1
             ? null
             : cart
-        ).filter((item) => item !== null)
-      );
-      };
+        )
+        .filter((item) => item !== null)
+    );
+  };
 
-      const getTotalItems = () => {
-        return cart.reduce((total, item) => {
-          return total + item.quantity;
-        }, 0);
-      };
-      
-      const cartTotal = cart.reduce((total, cartItem) => total + cartItem.price * cartItem.quantity, 0);
+  const getTotalItems = () => {
+    return cart.reduce((total, item) => {
+      return total + item.quantity;
+    }, 0);
+  };
 
+  const cartTotal = cart.reduce(
+    (total, cartItem) => total + cartItem.price * cartItem.quantity,
+    0
+  );
 
-    return (
-        <CartContext.Provider 
-        value={{
-            cart,
-            addItem,
-            removeItem,
-            clearCart,
-            incrementItem, 
-            decrementItem,
-            totalItems: getTotalItems(),
-            cartTotal,
-          }}
-        >
-            {children}
-        </CartContext.Provider>
-    )
-
-
-}
-
+  return (
+    <CartContext.Provider
+      value={{
+        cart,
+        addItem,
+        removeItem,
+        clearCart,
+        incrementItem,
+        decrementItem,
+        totalItems: getTotalItems(),
+        cartTotal,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
